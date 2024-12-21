73dfc10717689d640754518a64fedb2f668545cb
Dangkeys <66010360@kmitl.ac.th>
2024-12-21 15:20:07 +0700

    feat: update Interactor class to improve interaction handling and rename properties for clarity

--- Assets/Scripts/Core/Player/Interactor.cs
+++ Assets/Scripts/Core/Player/Interactor.cs
@@ -1,42 +1,94 @@
-using System;
 using UnityEngine;
+using System.Collections.Generic;
 
+/// <summary>
+/// Handles interaction with nearby interactable objects in the game world
+/// </summary>
 public class Interactor : MonoBehaviour
 {
-    [field: SerializeField] public InputReader InputReader { get; private set; }
-    [field: SerializeField] public float InteractRange { get; private set; }
+    [field: SerializeField, Tooltip("Reference to the input system")]
+    public InputReader InputReader { get; private set; }
 
-    void Start()
+    [field: SerializeField, Tooltip("Maximum distance at which interactions can occur")] 
+    public float InteractionRadius { get; private set; }
+
+    private void Start()
     {
-        InputReader.InteractEvent += TryToInteract;
+        InputReader.InteractEvent += HandleInteractionAttempt;
     }
 
-    private void TryToInteract()
+    private void HandleInteractionAttempt()
     {
-        Ray ray = new Ray(transform.position, transform.forward);
-        if (Physics.Raycast(ray, out RaycastHit hitInfo, InteractRange))
+        IInteractable nearestInteractable = FindNearestInteractable();
+        if (nearestInteractable != null)
         {
-            if (hitInfo.collider.gameObject.TryGetComponent(out IInteractable interactable))
-            {
-                interactable.Interact();
-            }
+            nearestInteractable.Interact();
         }
     }
 
-    // Draw debug visualization
-    private void OnDrawGizmos()
+    private IInteractable FindNearestInteractable()
     {
-        // Draw interaction range
-        Gizmos.color = Color.yellow;
-        Gizmos.DrawWireSphere(transform.position, InteractRange);
-        
-        // Draw raycast direction
-        Gizmos.color = Color.blue;
-        Gizmos.DrawRay(transform.position, transform.forward * InteractRange);
+        List<IInteractable> nearbyInteractables = FindInteractablesInRadius();
+        if (nearbyInteractables.Count == 0) return null;
+
+        return FindClosestFrom(nearbyInteractables);
     }
 
-    private void OnDestroy() 
+    private List<IInteractable> FindInteractablesInRadius()
     {
-        InputReader.InteractEvent -= TryToInteract;
+        List<IInteractable> interactables = new List<IInteractable>();
+        Collider[] nearbyColliders = Physics.OverlapSphere(transform.position, InteractionRadius);
+
+        foreach (Collider collider in nearbyColliders)
+        {
+            if (collider.TryGetComponent(out IInteractable interactable))
+            {
+                interactables.Add(interactable);
+            }
+        }
+        return interactables;
+    }
+
+    private IInteractable FindClosestFrom(List<IInteractable> interactables)
+    {
+        IInteractable closest = null;
+        float shortestDistance = float.MaxValue;
+
+        foreach (IInteractable interactable in interactables)
+        {
+            Vector3 interactablePosition = ((MonoBehaviour)interactable).transform.position;
+            float distance = Vector3.Distance(transform.position, interactablePosition);
+
+            if (distance < shortestDistance)
+            {
+                shortestDistance = distance;
+                closest = interactable;
+            }
+        }
+
+        return closest;
+    }
+
+    private void OnDrawGizmos()
+    {
+        DrawInteractionRadius();
+        DrawInteractionDirection();
+    }
+
+    private void DrawInteractionRadius()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, InteractionRadius);
+    }
+
+    private void DrawInteractionDirection()
+    {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawRay(transform.position, transform.forward * InteractionRadius);
+    }
+
+    private void OnDestroy()
+    {
+        InputReader.InteractEvent -= HandleInteractionAttempt;
     }
 }
