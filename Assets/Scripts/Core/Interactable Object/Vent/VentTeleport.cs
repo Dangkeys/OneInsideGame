815a10234611a1afc6f6d4e9be934a2215882e50
LuisPaprika <67010359@kmitl.ac.th>
2024-12-27 01:16:04 +0700

    feature/imprement vent

--- Assets/Scripts/Core/Interactable Object/Vent/VentTeleport.cs
+++ Assets/Scripts/Core/Interactable Object/Vent/VentTeleport.cs
@@ -1,8 +1,17 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
-public class VentTeleport : MonoBehaviour, IInteractable
+public class VentTeleport : MonoBehaviour
 {
-    public void Interact(){
-        
+    public Transform warpPosition;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.TryGetComponent<Player>(out Player player))
+        {
+            other.TryGetComponent<CharacterController>(out CharacterController cc);
+            cc.enabled = false;
+            player.transform.position = warpPosition.position;
+            cc.enabled = true;
+        }
     }
 }
