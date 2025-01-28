49969cc96cc494312b359e0bcc7b38b0f6c7cb6b
Dangkeys <66010360@kmitl.ac.th>
2025-01-28 10:51:11 +0700

    Refactor player spawning logic and improve variable naming for clarity

--- Assets/Scripts/Utilities/Az_Normal.cs
+++ Assets/Scripts/Utilities/Az_Normal.cs
@@ -6,24 +6,24 @@ public class Az_Normal : MonoBehaviour
      /// <summary>
      /// Returns the closest target to the given source position.
      /// </summary>
-     /// <param name="Src_Position">The source position.</param>
-     /// <param name="Targets">The targets.</param>
+     /// <param name="srcPosition">The source position.</param>
+     /// <param name="targets">The targets.</param>
      /// <returns>The closest target.</returns>
-     public static GameObject Get_Closet_Target(Vector3 Src_Position, List<GameObject> Targets)
+     public static GameObject Get_Closet_Target(Vector3 srcPosition, List<GameObject> targets)
      {
-          GameObject Closest_Target = null;
-          float Closest_Distance = Mathf.Infinity;
+          GameObject closestTarget = null;
+          float closestDistance = Mathf.Infinity;
 
-          foreach (GameObject Target in Targets)
+          foreach (GameObject target in targets)
           {
-               float Distance = Vector3.Distance(Target.transform.position, Src_Position);
-               if (Distance < Closest_Distance)
+               float Distance = Vector3.Distance(target.transform.position, srcPosition);
+               if (Distance < closestDistance)
                {
-                    Closest_Distance = Distance;
-                    Closest_Target = Target;
+                    closestDistance = Distance;
+                    closestTarget = target;
                }
           }
 
-          return Closest_Target;
+          return closestTarget;
      }
 }
