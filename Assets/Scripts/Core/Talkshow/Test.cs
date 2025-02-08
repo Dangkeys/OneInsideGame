70c0ba9bb68927675dbf9da7613b7be8ab668b1e
Dangkeys <66010360@kmitl.ac.th>
2025-02-08 14:22:25 +0700

    feat: add Test and TestScene scripts with network functionality and prefab updates

---
+++ Assets/Scripts/Core/Talkshow/Test.cs
@@ -0,0 +1,75 @@
+using QFSW.QC;
+using Unity.Netcode;
+using UnityEngine;
+
+public class Test : NetworkBehaviour
+{
+    private NetworkVariable<int> networkVariableNumber = new NetworkVariable<int>();
+    private int number = 0;
+    private int numberWithRPC = 0;
+
+
+    public override void OnNetworkSpawn()
+    {
+
+    }
+    void AddNumber()
+    {
+        number++;
+    }
+
+    [ServerRpc]
+    void AddNumberNetworkServerRpc()
+    {
+        networkVariableNumber.Value++;
+    }
+    void PrintNumber()
+    {
+        Debug.Log($"{OwnerClientId} : {number}");
+    }
+    void PrintNetworkVariableNumber()
+    {
+        Debug.Log($"{OwnerClientId} : {networkVariableNumber.Value}");
+    }
+    private void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            if (!IsOwner)
+                return;
+            AddNumber();
+            PrintNumber();
+        }
+        if (Input.GetKeyDown(KeyCode.N))
+        {
+            if (!IsOwner)
+                return;
+            AddNumberNetworkServerRpc();
+            PrintNetworkVariableNumber();
+        }
+
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            if (!IsOwner)
+                return;
+            TestScene.Instance.SetOwnershipServerRpc(OwnerClientId);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Y))
+        {
+            Debug.Log("Y");
+            if (!IsOwner)
+                return;
+            TestScene.Instance.SetOwnershipWithoutRequireOwnershipServerRpc(OwnerClientId);
+        }
+
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            Debug.Log("Normal Number");
+            PrintNumber();
+            Debug.Log("Network Variable Number");
+            PrintNetworkVariableNumber();
+        }
+    }
+}
---
+++ Assets/Scripts/Core/Talkshow/TestScene.cs
@@ -0,0 +1,73 @@
+using Unity.Netcode;
+using UnityEngine;
+
+public class TestScene : NetworkBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public static TestScene Instance { get; private set; }
+
+
+
+    private void Awake()
+    {
+        if(Instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        Instance = this;
+    }
+
+    void Start()
+    {
+        Debug.Log("Scene Started");
+    }
+    public override void OnNetworkSpawn()
+    {
+        foreach (var player in NetworkManager.Singleton.ConnectedClientsList)
+        {
+            Debug.Log("Player " + player.ClientId);
+        }
+
+        Debug.Log("TestScene  Spawned on Network with ownership of  " + OwnerClientId);
+
+        Debug.Log("Press A to increase normal number");
+        Debug.Log("Press N to increase network number");
+        Debug.Log("Press T to change scene ownership");
+        Debug.Log("Press Y to change scene ownership without require ownership");
+        Debug.Log("Press P to print all numbers"); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    [ServerRpc]
+    public void SetOwnershipServerRpc(ulong clientId)
+    {
+        SetOwnership(clientId);
+    }
+
+    [ServerRpc(RequireOwnership = false)]
+    public void SetOwnershipWithoutRequireOwnershipServerRpc(ulong clientId)
+    {
+        SetOwnership(clientId);
+    }
+
+
+    private void SetOwnership(ulong clientId)
+    {
+        if (gameObject.TryGetComponent(out NetworkObject networkObject))
+        {
+            networkObject.ChangeOwnership(clientId);
+            SetOwnershipClientRpc(clientId, new ClientRpcParams { Send = new ClientRpcSendParams { TargetClientIds = new[] { clientId } } });
+        }
+    }
+    [ClientRpc]
+    public void SetOwnershipClientRpc(ulong clientId, ClientRpcParams clientRpcParams = default)
+    {
+        Debug.Log("TestScene ownership changed to " + clientId);
+    }
+}
