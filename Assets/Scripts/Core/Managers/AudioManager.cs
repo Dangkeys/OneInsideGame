c3c2d1cb7f13427c63327f1b7ef9eab2763d4fb7
Dangkeys <66010360@kmitl.ac.th>
2025-02-13 22:09:26 +0700

    feat: add audio management system with SoundSO and SoundsCollectionSO

--- Assets/Scripts/Core/Managers/AudioManager.cs
+++ Assets/Scripts/Core/Managers/AudioManager.cs
@@ -1,16 +1,137 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class AudioManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+
+    [Range(0f, 2f)]
+    [SerializeField] private float masterVolume = 1f;
+    [SerializeField] private SoundsCollectionSO soundsCollectionSO;
+    [SerializeField] private AudioMixerGroup sfxMixerGroup;
+    [SerializeField] private AudioMixerGroup musicMixerGroup;
+
+    private AudioSource currentMusic;
+
+    #region Unity Methods
+
+    private void Start()
     {
-        
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnEnable()
     {
-        
+
     }
+
+    private void OnDisable()
+    {
+    }
+
+    #endregion
+
+    #region Sound Methods
+
+    private void PlayRandomSound(SoundSO[] sounds)
+    {
+        if (sounds != null && sounds.Length > 0)
+        {
+            SoundSO soundSO = sounds[Random.Range(0, sounds.Length)];
+            SoundToPlay(soundSO);
+        }
+    }
+
+    private void SoundToPlay(SoundSO soundSO)
+    {
+        AudioClip clip = soundSO.Clip;
+        float pitch = soundSO.Pitch;
+        float volume = soundSO.Volume * masterVolume;
+        bool loop = soundSO.Loop;
+        AudioMixerGroup audioMixerGroup;
+
+        pitch = RandomizePitch(soundSO, pitch);
+        audioMixerGroup = DetermineAudioMixerGroup(soundSO);
+
+        PlaySound(clip, pitch, volume, loop, audioMixerGroup);
+    }
+
+    private AudioMixerGroup DetermineAudioMixerGroup(SoundSO soundSO)
+    {
+        AudioMixerGroup audioMixerGroup;
+        switch (soundSO.AudioType)
+        {
+            case SoundSO.AudioTypes.SFX:
+                audioMixerGroup = sfxMixerGroup;
+                break;
+            case SoundSO.AudioTypes.Music:
+                audioMixerGroup = musicMixerGroup;
+                break;
+            default:
+                audioMixerGroup = null;
+                break;
+        }
+
+        return audioMixerGroup;
+    }
+
+    private static float RandomizePitch(SoundSO soundSO, float pitch)
+    {
+        if (soundSO.RandomizePitch)
+        {
+            float randomPitchModifier = Random.Range(-soundSO.RandomPitchRangeModifier, soundSO.RandomPitchRangeModifier);
+            pitch = soundSO.Pitch + randomPitchModifier;
+        }
+
+        return pitch;
+    }
+
+    private void PlaySound(AudioClip clip, float pitch, float volume, bool loop, AudioMixerGroup audioMixerGroup)
+    {
+        GameObject soundObject = new GameObject("Temp Audio Source");
+        AudioSource audioSource = soundObject.AddComponent<AudioSource>();
+        audioSource.clip = clip;
+        audioSource.pitch = pitch;
+        audioSource.volume = volume;
+        audioSource.loop = loop;
+        audioSource.outputAudioMixerGroup = audioMixerGroup;
+        audioSource.Play();
+
+        if (!loop)
+        { Destroy(soundObject, clip.length); }
+
+        DetermineMusic(audioMixerGroup, audioSource);
+    }
+
+    private void DetermineMusic(AudioMixerGroup audioMixerGroup, AudioSource audioSource)
+    {
+        if (audioMixerGroup == musicMixerGroup)
+        {
+            if (currentMusic != null)
+            {
+                currentMusic.Stop();
+            }
+
+            currentMusic = audioSource;
+        }
+    }
+
+
+    #endregion
+
+    #region SFX
+
+    private void PlayJumpSFX()
+    {
+        PlayRandomSound(soundsCollectionSO.Jump);
+    }
+    #endregion
+    #region Music
+
+    private void PlayMainMenuMusic()
+    {
+        PlayRandomSound(soundsCollectionSO.MainMenuMusic);
+    }
+
+    #endregion
 }
