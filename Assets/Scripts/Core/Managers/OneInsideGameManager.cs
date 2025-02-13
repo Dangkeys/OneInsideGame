3ea40c0e2a5b518e9afd7ed2553db76029821f87
Dangkeys <66010360@kmitl.ac.th>
2025-02-13 22:34:14 +0700

    feat: implement BootstrapUI and loading progress updates in OneInsideGameManager

--- Assets/Scripts/Core/Managers/OneInsideGameManager.cs
+++ Assets/Scripts/Core/Managers/OneInsideGameManager.cs
@@ -1,18 +1,38 @@
-
+using System;
 using Unity.Services.Authentication;
 using Unity.Services.Core;
 using Unity.Services.Vivox;
 using UnityEngine;
 
-
 public class OneInsideGameManager : SingletonPersistent<OneInsideGameManager>
 {
+    public event Action<float, string> OnBootstrapLoadingProgressChanged;
+
+    private readonly string[] loadingSteps = new[]
+    {
+        "Initializing Unity Services",
+        "Authenticating User",
+        "Initializing Vivox Service",
+        "Logging into Vivox Service",
+        "Loading Main Menu"
+    };
+
+    private int currentStep = 0;
+
     private CharacterManager characterManager;
     private PerkManager perkManager;
     private UserManager userManager;
     private AudioManager audioManager;
     private LobbyManager lobbyManager;
     private VoiceChatManager voiceChatManager;
+
+    private void UpdateProgress(string status)
+    {
+        float progress = (float)currentStep / loadingSteps.Length;
+        OnBootstrapLoadingProgressChanged?.Invoke(progress, status);
+        currentStep++;
+    }
+
     async void Start()
     {
         characterManager = GetComponentInChildren<CharacterManager>();
@@ -22,15 +42,20 @@ public class OneInsideGameManager : SingletonPersistent<OneInsideGameManager>
         voiceChatManager = GetComponentInChildren<VoiceChatManager>();
         lobbyManager = GetComponentInChildren<LobbyManager>();
 
+        UpdateProgress(loadingSteps[0]);
         await UnityServices.InitializeAsync();
-        await AuthenticationWrapper.DoAuth();
 
+        UpdateProgress(loadingSteps[1]);
+        await AuthenticationService.Instance.SignInAnonymouslyAsync();
         Debug.Log("Player Name: " + AuthenticationService.Instance.PlayerName);
 
+        UpdateProgress(loadingSteps[2]);
         await VivoxService.Instance.InitializeAsync();
 
+        UpdateProgress(loadingSteps[3]);
         await VivoxService.Instance.LoginAsync();
 
+        UpdateProgress(loadingSteps[4]);
         Loader.Load(GameScene.MainMenuScene);
     }
 }
