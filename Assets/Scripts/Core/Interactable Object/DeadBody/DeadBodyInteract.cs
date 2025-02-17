4a701e11d6bdc7ed7b69b114f81a85c77ca484bd
AzPepoze <65204787+AzPepoze@users.noreply.github.com>
2025-02-17 11:59:10 +0700

    feat: add DeadBodyInteract component for player interaction with dead bodies

---
+++ Assets/Scripts/Core/Interactable Object/DeadBody/DeadBodyInteract.cs
@@ -0,0 +1,28 @@
+using Unity.Netcode;
+using UnityEngine;
+
+public class DeadBodyInteract : NetworkBehaviour, IInteractable
+{
+    [SerializeField] private GameObject deadBody;
+
+    public void Interact(InteractionData interactionData)
+    {
+        if (deadBody.TryGetComponent<NetworkObject>(out NetworkObject deadBodyNetworkObject)
+            && interactionData.Interactor.TryGetComponent<NetworkObject>(out NetworkObject interactorNetworkObject))
+        {
+            ulong deadBodyOwnerID = deadBodyNetworkObject.OwnerClientId;
+            ulong interactorID = interactorNetworkObject.OwnerClientId;
+
+            Debug.Log($"Player {interactorID} found body of {deadBodyOwnerID}");
+        }
+    }
+
+    public bool CanInteract(InteractionData interactionData)
+    {
+        if (interactionData.Interactor.TryGetComponent<Player>(out Player player))
+        {
+            return player.IsAlive.Value;
+        }
+        return false;
+    }
+}
