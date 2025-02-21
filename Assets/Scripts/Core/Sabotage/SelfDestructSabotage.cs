f1e3db8cc3a53c497fc573efa0fa6de4e8d9e63d
LuisPaprika <67010359@kmitl.ac.th>
2025-02-21 20:32:39 +0700

    feature/implement countdown time when press sabortage button

---
+++ Assets/Scripts/Core/Sabotage/SelfDestructSabotage.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using Unity.Netcode;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class SelfDestructSabotage : NetworkBehaviour
+{
+    [SerializeField] private int delay;
+    [SerializeField] private TextMeshProUGUI timer;
+
+    public void StartCountdown()
+    {
+        timer.enabled = true;
+        StartCountdownServerRpc();
+    }
+
+    [ServerRpc]
+    private void StartCountdownServerRpc()
+    {
+        StartCoroutine(Countdown(delay));
+    }
+
+    IEnumerator Countdown(float delay)
+    {
+        float timeleft = delay;
+        while (timeleft > 0)
+        {
+            SetTimerTextServerRpc(timeleft);
+            Debug.Log(timeleft);
+            yield return new WaitForSeconds(1);
+            timeleft--;
+        }
+        timer.enabled = false;
+
+    }
+
+    [ServerRpc]
+    private void SetTimerTextServerRpc(float delay)
+    {
+        timer.text = delay.ToString();
+    }
+}
