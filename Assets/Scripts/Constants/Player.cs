afc0cf1896d6ca68d9292811b009ca77127601c3
AzPepoze <65204787+AzPepoze@users.noreply.github.com>
2025-02-28 20:27:49 +0700

    feat: add default player stats configuration for health and imposter attributes

--- Assets/Scripts/Constants/Player.cs
+++ Assets/Scripts/Constants/Player.cs
@@ -8,4 +8,20 @@ namespace OneInside.Constants
         public const int MAX_IMPOSTERS = 4;
         public const int MIN_IMPOSTERS = 1;
     }
+
+    public static class DefaultPlayerStatsConfig
+    {
+        public static class Player
+        {
+            public const float MAX_HEALTH = 3.0f;
+            public const float STUN_DURATION = 2.0f;
+        }
+
+        public static class Imposter
+        {
+            public const float ATTACK_COOLDOWN = 1.0f;
+            public const float DAMAGE = 1.0f;
+        }
+
+    }
 }
