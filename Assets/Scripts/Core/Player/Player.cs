0470809321a81310b30e94a534d18b57e9d698e9
AzPepoze <pladumguy@gmail.com>
2025-04-23 03:25:16 +0700

    fix: adjust MAX_HEALTH value and modify movement logic in PlayerState

--- Assets/Scripts/Core/Player/Player.cs
+++ Assets/Scripts/Core/Player/Player.cs
@@ -21,7 +21,7 @@ public class Player : NetworkBehaviour
 
     [Header("Status")]
     public NetworkVariable<bool> IsAlive = new NetworkVariable<bool>(true);
-            public NetworkVariable<PlayerRole> Role = new NetworkVariable<PlayerRole>(PlayerRole.None);
+    public NetworkVariable<PlayerRole> Role = new NetworkVariable<PlayerRole>(PlayerRole.None);
 
     [Header("Characters")]
     public NetworkVariable<FixedString64Bytes> CrewmateCharacterID = new NetworkVariable<FixedString64Bytes>();
